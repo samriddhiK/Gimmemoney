0fa33eb140a59b048de9c12a6b4e858abc4b9488
samriddhiK <75959569+samriddhiK@users.noreply.github.com>
2021-04-15 06:19:00 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("my first console project");
+var first_name = "Dabforthefamdab ";
+var second_name = "guest";
+var full_name = .first_name.concat(last_name);
+console.log (full_name);
+var a = 10;
+var b = 5;
+var add = a+b;
+console.log(add);
+
+var minus = a-b;
+console.log(minus);
+
+var multiply = a*b;
+console.log(multiply);
+
+var division = a/b;
+console.log(division);
